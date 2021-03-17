952d1edce3c24d8f110d811ec7bea5dd590dc519
1170301027 <1354922069@qq.com>
2021-03-17 21:28:56 +0800

    TrafficAnalysis

---
+++ src/main/java/org/example/work/flow/TrafficAnalysis.java
@@ -0,0 +1,14 @@
+package org.example.work.flow;
+
+import org.apache.http.HttpResponse;
+
+/**
+ * @Classname TrafficAnalysis
+ * @Description 流量解析
+ * @Date 2021/3/17 20:05
+ * @Created by shuaif
+ */
+public class TrafficAnalysis {
+    public static void responseAnalysis() {
+    }
+}
